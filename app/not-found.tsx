d1a945e5482b4704eaf9e911e286b2f717f0fb75
Shubham Prasad <61153966+SAMURAii-7@users.noreply.github.com>
2023-09-11 10:36:03 +0530

    make 404 page responsive

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -6,25 +6,27 @@ type Props = {};
 
 const NotFound = (props: Props) => {
     return (
-        <div className="mt-[80px] flex flex-row justify-between items-center w-full">
-            <div className="flex flex-col justify-center items-center">
-                <h1 className="text-8xl text-white">404</h1>
-                <p className="mt-[10px]">Page Not Found</p>
-                <Link href="/" className="mt-[50px]">
-                    <button className="bg-[#3f2ed5] p-[10px] rounded-[10px] w-[150px] hover:scale-105 duration-75">
-                        Go Back Home
-                    </button>
-                </Link>
+        <>
+            <div className="mt-[80px] flex flex-row justify-between items-center w-full max-md:flex-col-reverse">
+                <div className="flex flex-col justify-center items-center max-md:mt-[30px]">
+                    <h1 className="text-8xl text-white">404</h1>
+                    <p className="mt-[10px]">Page Not Found</p>
+                    <Link href="/" className="mt-[50px] max-md:mt-[30px]">
+                        <button className="bg-[#3f2ed5] p-[10px] rounded-[10px] w-[150px] hover:scale-105 duration-75">
+                            Go Back Home
+                        </button>
+                    </Link>
+                </div>
+                <Image
+                    src={pic}
+                    alt="404"
+                    width={400}
+                    height={200}
+                    quality={100}
+                    className="rounded-[50px]"
+                />
             </div>
-            <Image
-                src={pic}
-                alt="404"
-                width={400}
-                height={200}
-                quality={100}
-                className="rounded-[50px]"
-            />
-        </div>
+        </>
     );
 };
 
