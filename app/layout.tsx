7090198b4e8f62c14d8a8d64a79e00240a97a55b
Shubham Prasad <61153966+SAMURAii-7@users.noreply.github.com>
2023-09-14 14:02:20 +0530

    change favicon and do SEO optimization

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Montserrat } from "next/font/google";
-
 import Navbar from "@/app/components/Navbar";
 
 const montserrat = Montserrat({
@@ -11,7 +10,90 @@ const montserrat = Montserrat({
 
 export const metadata: Metadata = {
     title: "Shubham Prasad",
-    description: "Portfolio website of Shubham Prasad",
+    description:
+        "Software Engineer | Building Digital Worlds, One Line of Code at a Time",
+    openGraph: {
+        title: "Shubham Prasad",
+        description:
+            "Software Engineer | Building Digital Worlds, One Line of Code at a Time",
+        url: "https://shubhamprasad.vercel.app/",
+        siteName: "Shubham Prasad",
+        type: "website",
+        locale: "en_US",
+        images: [
+            {
+                url: "/opengraph-portfolio-logo.png",
+            },
+        ],
+    },
+    authors: [
+        {
+            name: "Shubham Prasad",
+        },
+    ],
+    creator: "Shubham Prasad",
+    twitter: {
+        card: "summary_large_image",
+        site: "@SAMURAii_7",
+        creator: "@SAMURAii_7",
+        title: "Shubham Prasad",
+        description:
+            "Software Engineer | Building Digital Worlds, One Line of Code at a Time",
+        images: ["/opengraph-portfolio-logo.png"],
+    },
+    keywords: [
+        "Shubham Prasad",
+        "Shubham",
+        "Prasad",
+        "Shubham Prasad Portfolio",
+        "Shubham Prasad Website",
+        "Shubham Prasad Developer",
+        "Shubham Prasad Software Engineer",
+        "Shubham Prasad Software Developer",
+        "Shubham Prasad Full Stack Developer",
+        "Shubham Prasad Full Stack Engineer",
+        "Shubham Prasad Full Stack",
+        "Shubham Prasad Full Stack Web Developer",
+        "Shubham Prasad Full Stack Web Engineer",
+        "Shubham Prasad Full Stack Web",
+        "Shubham Prasad Full Stack Developer",
+        "Shubham Prasad Full Stack Engineer",
+        "Shubham Prasad Full Stack",
+        "Shubham Prasad Full Stack Web Developer",
+        "Shubham Prasad Full Stack Web Engineer",
+        "Shubham Prasad Full Stack Web",
+        "Shubham Prasad Full Stack Developer",
+        "Shubham Prasad Full Stack Engineer",
+        "Shubham Prasad Full Stack",
+        "Shubham Prasad Full Stack Web Developer",
+        "Shubham Prasad Full Stack Web Engineer",
+        "Shubham Prasad Full Stack Web",
+    ],
+    robots: "index, follow",
+    icons: [
+        {
+            rel: "apple-touch-icon",
+            sizes: "180x180",
+            url: "/apple-touch-icon.png",
+        },
+        {
+            rel: "icon",
+            type: "image/png",
+            sizes: "32x32",
+            url: "/favicon-32x32.png",
+        },
+        {
+            rel: "icon",
+            type: "image/png",
+            sizes: "16x16",
+            url: "/favicon-16x16.png",
+        },
+    ],
+    manifest: "/site.webmanifest",
+    verification: {
+        google: "u7nSvwqDr2oWOwlJylnp6tDXX9IfaQThVm5fxhCGPgg",
+        me: "https://twitter.com/SAMURAii_7",
+    },
 };
 
 export default function RootLayout({
