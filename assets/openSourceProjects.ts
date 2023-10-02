4dac96d792796f98891bf0972f345d8b09ce4a4e
Shubham Prasad <61153966+SAMURAii-7@users.noreply.github.com>
2023-10-02 20:54:47 +0530

    change dark reader PR history link

--- assets/openSourceProjects.ts
+++ assets/openSourceProjects.ts
@@ -5,7 +5,7 @@ export const openSourceProjects = [
         name: "Dark Reader",
         description:
             "A browser extension that converts any website to dark mode",
-        link: "https://github.com/pulls?q=is:pr+author:SAMURAii-7+org:darkreader",
+        link: "https://github.com/darkreader/darkreader/pulls?q=is%3Apr+author%3ASAMURAii-7+is%3Aclosed",
         icon: DarkReaderIcon,
     },
 ];
