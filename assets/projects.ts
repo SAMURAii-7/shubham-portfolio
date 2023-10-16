773775e2bf05cd15cc33a61bd05587ae06b4202f
Shubham Prasad <61153966+SAMURAii-7@users.noreply.github.com>
2023-10-16 21:01:15 +0530

    configure prettier and format all the files

--- assets/projects.ts
+++ assets/projects.ts
@@ -1,42 +1,42 @@
 export const projects = [
-    {
-        name: "Shopping List",
-        description:
-            "A Full Stack progressive web application(PWA) to store items, create shopping lists and export them as a CSV file",
-        link: "https://create-shopping-list.vercel.app/",
-        github: "https://github.com/SAMURAii-7/shopping-list",
-        tech: ["react", "node", "express", "mongodb"],
-    },
-    {
-        name: "Network Management System",
-        description:
-            "A Full Stack web application which lets users add and delete devices along with their IP Address. Also has Sign Up and Login system with Two-Factor Authentication",
-        link: "https://nms-client.netlify.app/",
-        github: "https://github.com/SAMURAii-7/nms-backend",
-        tech: ["react", "springboot", "mongodb"],
-    },
-    {
-        name: "Bangalore Housing Price Prediction",
-        description:
-            "A Machine Learning project that predicts housing prices in Bangalore, India. A web application with a Flask server is used as a user interface",
-        link: "https://samuraii-7.github.io/bangalore-house-price-predictor/client/app.html",
-        github: "https://github.com/SAMURAii-7/bangalore-house-price-predictor",
-        tech: ["python", "flask"],
-    },
-    {
-        name: "Github Finder",
-        description:
-            "A web application made using React to find a Github user's profile and display its details by making requests to the Github API",
-        link: "https://github-finder-nine-beige.vercel.app/",
-        github: "https://github.com/SAMURAii-7/github-finder",
-        tech: ["react"],
-    },
-    {
-        name: "Employee Management System",
-        description:
-            "A Java application with GUI made using JavaFX to create, update, view and delete employee records and store them in a MySQL database using XAMPP",
-        link: "",
-        github: "https://github.com/SAMURAii-7/Emp-Mgmt-Sys",
-        tech: ["java", "mysql", "javafx"],
-    },
+  {
+    name: "Shopping List",
+    description:
+      "A Full Stack progressive web application(PWA) to store items, create shopping lists and export them as a CSV file",
+    link: "https://create-shopping-list.vercel.app/",
+    github: "https://github.com/SAMURAii-7/shopping-list",
+    tech: ["react", "node", "express", "mongodb"],
+  },
+  {
+    name: "Network Management System",
+    description:
+      "A Full Stack web application which lets users add and delete devices along with their IP Address. Also has Sign Up and Login system with Two-Factor Authentication",
+    link: "https://nms-client.netlify.app/",
+    github: "https://github.com/SAMURAii-7/nms-backend",
+    tech: ["react", "springboot", "mongodb"],
+  },
+  {
+    name: "Bangalore Housing Price Prediction",
+    description:
+      "A Machine Learning project that predicts housing prices in Bangalore, India. A web application with a Flask server is used as a user interface",
+    link: "https://samuraii-7.github.io/bangalore-house-price-predictor/client/app.html",
+    github: "https://github.com/SAMURAii-7/bangalore-house-price-predictor",
+    tech: ["python", "flask"],
+  },
+  {
+    name: "Github Finder",
+    description:
+      "A web application made using React to find a Github user's profile and display its details by making requests to the Github API",
+    link: "https://github-finder-nine-beige.vercel.app/",
+    github: "https://github.com/SAMURAii-7/github-finder",
+    tech: ["react"],
+  },
+  {
+    name: "Employee Management System",
+    description:
+      "A Java application with GUI made using JavaFX to create, update, view and delete employee records and store them in a MySQL database using XAMPP",
+    link: "",
+    github: "https://github.com/SAMURAii-7/Emp-Mgmt-Sys",
+    tech: ["java", "mysql", "javafx"],
+  },
 ];
--- assets/skills.ts
+++ assets/skills.ts
@@ -1,56 +1,65 @@
 export const skills = [
-    {
-        name: "React",
-        source: "https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg",
-        link: "https://react.dev/",
-        isDark: false,
-    },
-    {
-        name: "Next.js",
-        source: "https://raw.githubusercontent.com/devicons/devicon/master/icons/nextjs/nextjs-original-wordmark.svg",
-        link: "https://nextjs.org/",
-        isDark: true,
-    },
-    {
-        name: "Angular",
-        source: "https://raw.githubusercontent.com/devicons/devicon/master/icons/angularjs/angularjs-original.svg",
-        link: "https://angular.io/",
-        isDark: false,
-    },
-    {
-        name: "Spring Boot",
-        source: "https://raw.githubusercontent.com/devicons/devicon/master/icons/spring/spring-original-wordmark.svg",
-        link: "https://spring.io/projects/spring-boot",
-        isDark: false,
-    },
-    {
-        name: "Express.js",
-        source: "https://raw.githubusercontent.com/devicons/devicon/master/icons/express/express-original-wordmark.svg",
-        link: "https://expressjs.com/",
-        isDark: true,
-    },
-    {
-        name: "Java",
-        source: "https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original-wordmark.svg",
-        link: "https://www.java.com/",
-        isDark: false,
-    },
-    {
-        name: "Python",
-        source: "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original-wordmark.svg",
-        link: "https://www.python.org/",
-        isDark: false,
-    },
-    {
-        name: "MongoDB",
-        source: "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
-        link: "https://www.mongodb.com/",
-        isDark: false,
-    },
-    {
-        name: "MySQL",
-        source: "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
-        link: "https://www.mysql.com/",
-        isDark: false,
-    },
+  {
+    name: "React",
+    source:
+      "https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg",
+    link: "https://react.dev/",
+    isDark: false,
+  },
+  {
+    name: "Next.js",
+    source:
+      "https://raw.githubusercontent.com/devicons/devicon/master/icons/nextjs/nextjs-original-wordmark.svg",
+    link: "https://nextjs.org/",
+    isDark: true,
+  },
+  {
+    name: "Angular",
+    source:
+      "https://raw.githubusercontent.com/devicons/devicon/master/icons/angularjs/angularjs-original.svg",
+    link: "https://angular.io/",
+    isDark: false,
+  },
+  {
+    name: "Spring Boot",
+    source:
+      "https://raw.githubusercontent.com/devicons/devicon/master/icons/spring/spring-original-wordmark.svg",
+    link: "https://spring.io/projects/spring-boot",
+    isDark: false,
+  },
+  {
+    name: "Express.js",
+    source:
+      "https://raw.githubusercontent.com/devicons/devicon/master/icons/express/express-original-wordmark.svg",
+    link: "https://expressjs.com/",
+    isDark: true,
+  },
+  {
+    name: "Java",
+    source:
+      "https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original-wordmark.svg",
+    link: "https://www.java.com/",
+    isDark: false,
+  },
+  {
+    name: "Python",
+    source:
+      "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original-wordmark.svg",
+    link: "https://www.python.org/",
+    isDark: false,
+  },
+  {
+    name: "MongoDB",
+    source:
+      "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+    link: "https://www.mongodb.com/",
+    isDark: false,
+  },
+  {
+    name: "MySQL",
+    source:
+      "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+    link: "https://www.mysql.com/",
+    isDark: false,
+  },
 ];
