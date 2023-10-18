015132754f089ca32d3080dcb1d28f615d8016da
Shubham Prasad <61153966+SAMURAii-7@users.noreply.github.com>
2023-10-18 22:57:22 +0530

    modify project card tech stack styling and add new projects

--- app/components/project-component/ProjectTechStack.tsx
+++ app/components/project-component/ProjectTechStack.tsx
@@ -2,7 +2,11 @@ import React from "react";
 import Image from "next/image";
 import { FaReact, FaNodeJs } from "react-icons/fa";
 import {
+  SiTailwindcss,
+  SiPrisma,
+  SiTypescript,
   SiExpress,
+  SiPostgresql,
   SiMongodb,
   SiSpringboot,
   SiFlask,
@@ -10,6 +14,7 @@ import {
 } from "react-icons/si";
 import PyLogo from "@/assets/python.svg";
 import JavaLogo from "@/assets/java.svg";
+import NextJsLogo from "@/assets/nextjs.svg";
 
 type Props = {
   tech: string[];
@@ -17,22 +22,45 @@ type Props = {
 
 // Create a mapping of technology names to their corresponding icons/images
 const techIcons: Record<string, React.ReactNode> = {
-  react: <FaReact className="m-[5px] text-lg text-[#61dafb]" />,
-  node: <FaNodeJs className="m-[5px] text-lg text-[#3c873a]" />,
-  express: <SiExpress className="m-[5px] text-lg" />,
-  mongodb: <SiMongodb className="m-[5px] text-lg text-[#3fa037]" />,
-  springboot: <SiSpringboot className="m-[5px] text-lg text-[#6db33f]" />,
-  python: <Image src={PyLogo} alt="python-icon" width={35} height={35} />,
-  flask: <SiFlask className="m-[5px] text-lg" />,
-  java: <Image src={JavaLogo} alt="java-icon" width={35} height={35} />,
-  mysql: <SiMysql className="m-[5px] text-3xl text-[#00758f]" />,
-  javafx: <span className="m-[5px] text-xs text-[#d4d4d4]">JavaFx</span>,
+  React: <FaReact className="m-1 text-lg text-[#61dafb]" />,
+  "Next.js": (
+    <Image src={NextJsLogo} alt="nextjs-icon" width={20} height={20} />
+  ),
+  TypeScript: (
+    <SiTypescript className="m-1 rounded-sm bg-white text-lg text-[#007acc]" />
+  ),
+  "Tailwind CSS": <SiTailwindcss className="m-1 text-lg text-[#06b6d4]" />,
+  Prisma: <SiPrisma className="m-1 text-lg text-[#d4d4d4]" />,
+  "Node.js": <FaNodeJs className="m-1 text-lg text-[#3c873a]" />,
+  "Express.js": <SiExpress className="m-1 text-lg" />,
+  PostgreSQL: <SiPostgresql className="m-1 text-lg text-[#d4d4d4]" />,
+  MongoDB: <SiMongodb className="m-1 text-lg text-[#3fa037]" />,
+  "Spring Boot": <SiSpringboot className="m-1 text-lg text-[#6db33f]" />,
+  Python: <Image src={PyLogo} alt="python-icon" width={35} height={35} />,
+  Flask: <SiFlask className="m-1 text-lg" />,
+  Java: <Image src={JavaLogo} alt="java-icon" width={35} height={35} />,
+  MySQL: <SiMysql className="m-1 text-3xl text-[#00758f]" />,
+  "Java FX": <span className="m-1 text-xs text-[#d4d4d4]">JavaFx</span>,
 };
 
 const ProjectTechStack = ({ tech }: Props) => {
   return (
-    <div className="flex flex-row items-center pt-[5px]">
-      {tech.map((t) => techIcons[t])}
+    <div className="flex flex-row items-center pt-2">
+      {tech.map((t) => (
+        <div
+          key={t}
+          className="m-1 flex flex-row items-center justify-center rounded-3xl bg-[#3d3d3d] p-1"
+        >
+          {techIcons[t]}
+          <div
+            className={`hidden p-1 text-xs ${
+              t === "Java FX" ? "lg:hidden" : "lg:block"
+            }`}
+          >
+            {t}
+          </div>
+        </div>
+      ))}
     </div>
   );
 };
--- assets/projects.ts
+++ assets/projects.ts
@@ -2,18 +2,26 @@ export const projects = [
   {
     name: "Shopping List",
     description:
-      "A Full Stack progressive web application(PWA) to store items, create shopping lists and export them as a CSV file",
+      "A Full-Stack progressive web application(PWA) to store items, create shopping lists and export them as a CSV file",
     link: "https://create-shopping-list.vercel.app/",
     github: "https://github.com/SAMURAii-7/shopping-list",
-    tech: ["react", "node", "express", "mongodb"],
+    tech: ["React", "Node.js", "Express.js", "MongoDB"],
+  },
+  {
+    name: "E-commerce Platform",
+    description:
+      "An E-commerce platform that enables users to search for products, manage their cart by adding, deleting, and adjusting item quantities. Additionally, it offers Google login integration, seamlessly retaining items in the cart from the pre-login session.",
+    link: "https://nextjs-ecommerce-omega-sand.vercel.app",
+    github: "https://github.com/SAMURAii-7/nextjs-ecommerce",
+    tech: ["Next.js", "TypeScript", "PostgreSQL", "Prisma", "Tailwind CSS"],
   },
   {
     name: "Network Management System",
     description:
-      "A Full Stack web application which lets users add and delete devices along with their IP Address. Also has Sign Up and Login system with Two-Factor Authentication",
+      "A Full-Stack web application which lets users add and delete devices along with their IP Address. Also has Sign Up and Login system with Two-Factor Authentication",
     link: "https://nms-client.netlify.app/",
     github: "https://github.com/SAMURAii-7/nms-backend",
-    tech: ["react", "springboot", "mongodb"],
+    tech: ["React", "Spring Boot", "MongoDB"],
   },
   {
     name: "Bangalore Housing Price Prediction",
@@ -21,7 +29,7 @@ export const projects = [
       "A Machine Learning project that predicts housing prices in Bangalore, India. A web application with a Flask server is used as a user interface",
     link: "https://samuraii-7.github.io/bangalore-house-price-predictor/client/app.html",
     github: "https://github.com/SAMURAii-7/bangalore-house-price-predictor",
-    tech: ["python", "flask"],
+    tech: ["Python", "Flask"],
   },
   {
     name: "Github Finder",
@@ -29,7 +37,7 @@ export const projects = [
       "A web application made using React to find a Github user's profile and display its details by making requests to the Github API",
     link: "https://github-finder-nine-beige.vercel.app/",
     github: "https://github.com/SAMURAii-7/github-finder",
-    tech: ["react"],
+    tech: ["React"],
   },
   {
     name: "Employee Management System",
@@ -37,6 +45,6 @@ export const projects = [
       "A Java application with GUI made using JavaFX to create, update, view and delete employee records and store them in a MySQL database using XAMPP",
     link: "",
     github: "https://github.com/SAMURAii-7/Emp-Mgmt-Sys",
-    tech: ["java", "mysql", "javafx"],
+    tech: ["Java", "MySQL", "Java FX"],
   },
 ];
